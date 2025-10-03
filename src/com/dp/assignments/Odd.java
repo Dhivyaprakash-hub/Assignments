674e6c5c5e60bd46c8456f111ac39713da276895
Dhivyaprakash N <ndhivyaprakash30042000@gmail.com>
2025-10-03 12:35:01 +0530

    Added Odd.java and .gitignore

---
+++ src/com/dp/assignments/Odd.java
@@ -0,0 +1,15 @@
+package com.dp.assignments;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		for(int i= 1; i <= 20; i =i + 1) {
+			if(i%2 == 1) {
+			System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
